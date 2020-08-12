845264ec778bb53b427f312eda2c9e252eb314d6
twber <hctsai@umich.edu>
2020-08-12 22:42:53 +0800

    Initial Commit

---
+++ zodiac year/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  zodiac year
+//
+//  Created by Bernice TSAI on 2020/8/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
